0198693d115451a1241dbbe29602d5bde8296300
aakashdeveloper <ahanda205@gmail.com>
2021-02-10 21:58:43 +0530

    A

--- dashboard/app.js
+++ dashboard/app.js
@@ -7,3 +7,136 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongourl="mongodb://localhost:27017";
 let db;
+let col_name="users";
+
+app.use(cors());
+//parse data for post call
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+//health Check
+app.get('/',(req,res) => {
+    res.status(200).send("Health OK")
+});
+
+//postUser
+app.post('/addUser',(req,res) => {
+    console.log(req.body);
+    db.collection(col_name).insert(req.body,(err,result) =>{
+        if(err) throw err;
+        res.status(200).send('Data Added')
+    })
+})
+
+//getUser
+app.get('/users',(req,res) => {
+    var query = {}
+    if(req.query.city && req.query.role){
+        query={city:req.query.city,role:req.query.role,isActive:true}
+    }
+    else if(req.query.city){
+        query={city:req.query.city,isActive:true}
+    }else if(req.query.role){
+        query={role:req.query.role,isActive:true}
+    }
+    else{
+        let isAct;
+        if(req.query.isActive=="false"){
+            isAct= false
+        }else{
+            isAct= true
+        }
+        query={isActive:req.query.isActive?isAct:true}
+    }
+    db.collection(col_name).find(query).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//updatUser
+app.put('/updateUser',(req,res) => {
+    let id = req.body._id;
+    if(req.body.phone.length<10){
+        res.send('invalid number')
+    }
+    db.collection(col_name).update(
+        {_id:mongo.ObjectID(id)},
+        {
+            $set:{
+                name: req.body.name,
+                city: req.body.city,
+                phone: req.body.phone,
+                isActive: req.body.isActive?req.body.isActive:true,
+                role: req.body.role,
+                email: req.body.email
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send('Data Updated')
+        }
+    )
+})
+
+//harddelete
+app.delete('/deleteUser',(req,res) => {
+    let Id = mongo.ObjectID(req.body._id);
+    db.collection(col_name).remove({_id:Id},(err,result) =>{
+        if(err) throw err;
+        res.send("Data Deleted")
+    })
+})
+
+//deactivate(soft delete)
+app.put('/deactivateUser',(req,res) => {
+    let id = req.body._id;
+    db.collection(col_name).update(
+        {_id:mongo.ObjectID(id)},
+        {
+            $set:{
+                isActive: false
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send('User Deactivated')
+        }
+    )
+})
+//activate
+app.put('/activateUser',(req,res) => {
+    let id = req.body._id;
+    db.collection(col_name).update(
+        {_id:mongo.ObjectID(id)},
+        {
+            $set:{
+                isActive: true
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send('User activated')
+        }
+    )
+})
+
+//getProfile
+app.get('/user/:id',(req,res)=>{
+    var id = mongo.ObjectID(req.params.id)
+    var query = {}
+    query ={_id:id}
+    db.collection(col_name).findOne(query,(err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//connect with db
+MongoClient.connect(mongourl,(err,connection)=>{
+    if(err) console.log(err);
+    db=connection.db('febnode');
+});
+
+//Start server
+app.listen(port,(err)=> {
+    if(err) throw err;
+    console.log(`Server is running on port ${port}`)
+})
